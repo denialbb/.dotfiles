e3b44443d92dc0f4a7ca95e52862eba37b7c764f
Daniel Biasiotto <daniel.biasiotto@edu.unito.it>
2025-11-10 12:24:18 +0100

    migrated nvim to its own repo

--- .config/nvim/init.lua
+++
@@ -1,5 +0,0 @@
--- ~/.config/nvim/init.lua (top of file)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
--- .config/nvim/lua/plugins/disabled.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { "bufferline.nvim", enabled = false },
-  { "nvim-tree/nvim-tree.lua", enabled = false },
-  { "yetone/avante.nvim", enabled = false },
-  { "nvim-mini/mini.animate", enabled = false },
-  { "hrsh7th/nvim-cmp", enabled = false },
-}
