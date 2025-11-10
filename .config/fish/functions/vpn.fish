440f88a2e0f03eaf4fdc78385dab5bcfb4fc8fed
Daniel Biasiotto <mail@denial.in.net>
2025-11-10 21:01:20 +0100

    rm hypr conflicts and add fishes

--- .config/fish/functions/vpn.fish
+++ .config/fish/functions/vpn.fish
@@ -1,3 +1,3 @@
 function vpn
-    sudo openvpn --config ~/.config/openvpn/us-free-5.protonvpn.udp.ovpn --auth-user-pass ~/.config/openvpn/pass.txt &
+    sudo openvpn --config ~/.config/openvpn/us-free-5.protonvpn.udp.ovpn --auth-user-pass ~/.config/openvpn/pass.txt --daemon
 end
