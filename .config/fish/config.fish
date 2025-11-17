d7434df3bd195f4800597354d33aaaa665c83684
Daniel Biasiotto <daniel.biasiotto@edu.unito.it>
2025-11-17 09:40:49 +0100

    ghostty+fish+zellij working seamlessly now

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set fish_greeting ''
+
 starship init fish | source
 zoxide init fish | source
 mise activate fish | source
@@ -9,8 +11,15 @@ mise activate fish | source
 export PATH="$PATH:~/.config/emacs/bin"
 export PATH="$PATH:~/.local/bin"
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
-export BROWSER="zen-browser"
-set fish_greeting ''
+export BROWSER="librewolf"
+
+export ZELLIJ_CONFIG_DIR=$HOME/.config/zellij
+
+# Check if our Terminal emulator is Ghostty
+if [ "$TERM" = xterm-ghostty ]
+    # Launch zellij
+    eval (zellij setup --generate-auto-start fish | string collect)
+end
 
 for f in ~/.config/fish/user/*.fish
     source $f
